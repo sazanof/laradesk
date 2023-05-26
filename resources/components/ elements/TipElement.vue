98e6d01846b9666fa6f80c188e68ecf6432944e5
Mikhail Sazanov <m@sazanof.ru>
2023-05-26 22:45:20 +0300

    up

---
+++ resources/components/ elements/TipElement.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+        :class="`tip alert alert-${type}`">
+        <div class="icon">
+            <slot name="icon">
+                <LightbulbOnIcon :size="48" />
+            </slot>
+        </div>
+        <div class="content">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+import LightbulbOnIcon from 'vue-material-design-icons/LightbulbOn.vue'
+
+export default {
+    name: 'TipElement',
+    components: {
+        LightbulbOnIcon
+    },
+    props: {
+        type: {
+            type: String,
+            default: 'info'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.tip {
+    border-radius: var(--border-radius);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: var(--padding-box);
+
+    .icon {
+        margin-right: 10px;
+    }
+
+    .content {
+    }
+}
+</style>
--- resources/components/pages/Adminitration/FormManagement.vue
+++ resources/components/pages/Adminitration/FormManagement.vue
@@ -1,11 +1,53 @@
-<template>Form Man {{ id }}</template>
+<template>
+    <div
+        v-if="category"
+        class="form-management">
+        <h3>
+            {{
+                $t('Manage category {category}', {
+                    category: category.name
+                })
+            }}
+        </h3>
+        <TipElement class="mt-3">
+            {{
+                $t('You can select the required fields for the application form and change the order of their location. Please note that the default fields cannot be deleted.')
+            }}
+        </TipElement>
+        <div class="fields-draggable">
+            <div class="fields-available">
+                <div class="title">
+                    {{ $t('Available fields') }}
+                </div>
+                <div class="fields-list">
+                    {{ fields }}
+                </div>
+            </div>
+            <div class="fields-enabled">
+                <div class="title">
+                    {{ $t('Form fields') }}
+                </div>
+                <div class="fields-list">
+                    {{ fields }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 
 <script>
+import TipElement from '../../ elements/TipElement.vue'
+
 export default {
     name: 'FormManagement',
+    components: {
+        TipElement
+    },
     data() {
         return {
-            category: null
+            category: null,
+            availableFields: [],
+            enabledFields: []
         }
     },
     computed: {
@@ -27,12 +69,33 @@ export default {
             }
         },
         async getCategory() {
-            return this.$store.dispatch('getCategoryWithFields', this.id)
+            this.category = await this.$store.dispatch('getCategoryWithFields', this.id)
         }
     }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.fields-draggable {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 
+    .title {
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 10px;
+    }
+
+    .fields-available {
+        width: 50%;
+        padding: var(--padding-box);
+        border-right: 1px solid var(--bs-border-color);
+    }
+
+    .fields-enabled {
+        width: 50%;
+        padding: var(--padding-box);
+    }
+}
 </style>
