260de23fa9874af729966a629896186fbdeb8ebd
Mikhail Sazanov <m@sazanof.ru>
2023-09-15 07:53:40 +0300

    fixup! background

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{$favicon}}" type="image/jpeg">
+    <link rel="icon" href="{{$favicon}}" type="image/x-icon">
     <title>{{$name}}</title>
 
 
