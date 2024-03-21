1542fd733efdb992cdef17b7b43d988fbf452d8a
Mikhail Sazanov <m@sazanof.ru>
2024-03-21 13:11:10 +0300

    enh! custom error pages

--- resources/js/error.js
+++ resources/js/error.js
@@ -1,2 +1,7 @@
 import '../css/app.scss'
 import './bootstrap'
+import '../css/error-page.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('errorPage').style.display = 'flex'
+})
