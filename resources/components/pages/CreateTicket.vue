64ed1347d526bec43db68506671ceae075a6bcad
Mikhail Sazanov <m@sazanof.ru>
2024-06-05 07:18:40 +0300

    fixup!

--- resources/components/pages/CreateTicket.vue
+++ resources/components/pages/CreateTicket.vue
@@ -221,9 +221,9 @@ export default {
                 failed = true
             }
             if (this.subject.length < 3 || this.contentText < 3) {
-                faled = true
+                failed = true
             }
-            сonst errorFields = this.categoryFields.filter(f => f.required === 1)
+            const errorFields = this.categoryFields.filter(f => f.required === 1)
             errorFields.map(err => {
                 const existing = this.fieldsData.find(_f => _f.category_field_id === err.category_field_id)
                 if (existing === undefined || existing.value === null || existing.value === '') {
